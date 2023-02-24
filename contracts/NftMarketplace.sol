87fe2c5b5d49a09f53aa43bc2097f6fb4384269a
Francisco Freire <franciscocfreire@gmail.com>
2023-02-24 17:56:36 -0300

    Add updateListing function

--- contracts/NftMarketplace.sol
+++ contracts/NftMarketplace.sol
@@ -161,6 +161,17 @@ contract NftMarketPlace is ReentrancyGuard {
         delete (s_listings[nftAddress][tokenId]);
         emit ItemCanceled(msg.sender, nftAddress, tokenId);
     }
+
+    function updateListing(
+        address nftAddress,
+        uint256 tokenId,
+        uint256 newPrice
+    ) external isListed(nftAddress,tokenId) isOwner(nftAddress, tokenId, msg.sender){
+
+        s_listings[nftAddress][tokenId].price = newPrice;
+        emit ItemListed(msg.sender, nftAddress, tokenId, newPrice);
+
+    }
 }
 
 // 1. `listItem`: List NFTs on the marketplace
